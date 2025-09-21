b6713da161d67767206f65ee1c8595324e2a629b
Kang Tae Hyun <126179088+polyglot-k@users.noreply.github.com>
2025-09-22 00:37:44 +0900

    Change sort property name to Korean in fetchNotionItems

--- notion-to-discord.js
+++ notion-to-discord.js
@@ -46,7 +46,7 @@ async function fetchNotionItems(dataSourceId) {
   const response = await axios.post(
     `https://api.notion.com/v1/data_sources/${dataSourceId}/query`,
     {
-      sorts: [{ property: "Created time", direction: "ascending" }],
+      sorts: [{ property: "생성 일시", direction: "ascending" }],
     },
     {
       headers: {
